2540fe8961bd8af38bc91d7d6b5e21a865c63abf
Ikram Maulana <ikram075@ummi.ac.id>
2023-09-27 19:08:14 +0700

    Chore: Add About Section

--- src/components/custom-texts.tsx
+++ src/components/custom-texts.tsx
@@ -1,8 +1,47 @@
-const TypingText = () => {
-  return <div>Typing Text</div>;
+"use client";
+
+import { cn } from "@/lib/utils";
+import { textContainer, textVariant2 } from "@/utils/motion";
+import { motion } from "framer-motion";
+
+const TypingText = ({
+  title,
+  textStyles,
+}: {
+  title: string;
+  textStyles: string;
+}) => {
+  return (
+    <motion.p
+      variants={textContainer}
+      className={cn(
+        `${textStyles}`,
+        "font-normal text-sm text-secondary-white"
+      )}
+    >
+      {Array.from(title).map((letter, index) => (
+        <motion.span
+          key={index}
+          variants={textVariant2}
+          className={cn(
+            `${textStyles}`,
+            "font-normal text-sm text-secondary-white"
+          )}
+        >
+          {letter === " " ? "\u00A0" : letter}
+        </motion.span>
+      ))}
+    </motion.p>
+  );
 };
 
-const TitleText = () => {
+const TitleText = ({
+  title,
+  textStyles,
+}: {
+  title: string;
+  textStyles: string;
+}) => {
   return <div>Title Text</div>;
 };
 
--- src/components/sections/about.tsx
+++ src/components/sections/about.tsx
@@ -1,7 +1,64 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import styles from "@/styles";
+import { fadeIn, staggerContainer } from "@/utils/motion";
+import { motion } from "framer-motion";
 import { FC } from "react";
+import { TypingText } from "../custom-texts";
+import Image from "next/image";
 
 const About: FC = () => {
-  return <section>About Section</section>;
+  return (
+    <section className={cn(`${styles.paddings}`, "relative z-20")}>
+      <div className="gradient-02 z-0" />
+      <motion.div
+        variants={staggerContainer(0.5, 0.1)}
+        initial="hidden"
+        whileInView="show"
+        viewport={{
+          once: false,
+          amount: 0.25,
+        }}
+        className={cn(
+          `${styles.innerWidth} ${styles.flexCenter}`,
+          "mx-auto flex-col"
+        )}
+      >
+        <TypingText title="| About Metaversus" textStyles="text-center" />
+
+        <motion.p
+          variants={fadeIn("up", "tween", 0.2, 1)}
+          className="mt-2 font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
+        >
+          <span className="font-extrabold text-white">Metaverse</span> is a new
+          thing in the future, where you can enjoy the virtual world by feeling
+          like it&#39;s really real, you can feel what you feel in this
+          metaverse world, because this is really the{" "}
+          <span className="font-extrabold text-white">
+            madness of the metaverse
+          </span>{" "}
+          of today, using only{" "}
+          <span className="font-extrabold text-white">VR</span> devices you can
+          easily explore the metaverse world you want, turn your dreams into
+          reality. Let&#39;s{" "}
+          <span className="font-extrabold text-white">explore</span> the madness
+          of the metaverse by scrolling down
+        </motion.p>
+
+        <motion.div variants={fadeIn("up", "tween", 0.3, 1)} className="mt-7">
+          <Image
+            src="/arrow-down.svg"
+            alt="arrow-down"
+            width={18}
+            height={28}
+            className="w-[18px] h-7 object-contain"
+          />
+        </motion.div>
+      </motion.div>
+      About Section
+    </section>
+  );
 };
 
 export default About;
